11ac5755540586c7de77b88ecf33255903932b77
Jeshon Assunçao <jeshon.assuncao@he-arc.ch>
2016-04-05 14:09:46 +0200

    Make a simple Film service Only one method :  - GetFilmList

---
+++ ServiceFMD/ServiceFMD/Film.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServiceFMD
+{
+    [DataContract]
+    public class Film
+    {
+        [DataMember]
+        public int FilmID { get; set; }
+
+        [DataMember]
+        public string FilmName { get; set; }
+
+        [DataMember]
+        public string FilmLink { get; set; }
+
+        [DataMember]
+        public float FilmProgression { get; set; }
+    }
+}
---
+++ ServiceFMD/ServiceFMD/IFilmRESTService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceFMD
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IFilmRESTService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IFilmRESTService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetFilmList/")]
+        List<Film> GetFilmList();
+    }
+}
